819311f919073d597192528200a7e5d9cce6a9f2
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-11-09 14:41:39 +0100

    using all string methods in a coding challenge

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -395,3 +395,47 @@ const annuncement = 'All passenger come to door 23. come to door 23!';
 console.log(annuncement.replace(/door/g, 'gate'));
 
 console.log(annuncement.replaceAll('door', 'gate'));
+
+// Coding Challenge #4
+// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase. The input will come from a textarea inserted into the DOM (see code below to insert the elements), and conversion will happen when the button is pressed.
+// Test data (pasted to textarea, including spaces):
+
+// underscore_case;
+// first_name;
+// Some_Variable;
+// calculate_AGE;
+// delayed_departure;
+
+// Should produce this output (5 separate console.log outputs):
+
+// underscoreCase ✅
+// firstName ✅✅
+// someVariable ✅✅✅
+// calculateAge ✅✅✅✅
+// delayedDeparture ✅✅✅✅✅
+
+// Hints:
+// § Remember which character defines a new line in the textarea
+// § The solution only needs to work for a variable made out of 2 words, like a_b
+// § Start without worrying about the ✅. Tackle that only after you have the variable name conversion working
+// § This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue! Afterwards, test with your own test data!
+
+document.body.append(document.createElement('textarea'));
+
+document.body.append(document.createElement('button'));
+
+const sendbutton = document.querySelector('button');
+
+sendbutton.addEventListener('click', function () {
+  const textArea = document.querySelector('textarea').value;
+  const singelLines = textArea.split(/\n/g);
+  let ind = 1;
+
+  for (const [i, line] of singelLines.entries()) {
+    const [startWord, endWord] = line.trim().toLowerCase().split('_');
+
+    const oneWord =
+      startWord + endWord.replace(endWord[0], endWord[0].toUpperCase());
+    console.log(oneWord.replace('_', ' ').padEnd(20) + `${'✅'.repeat(i + 1)}`);
+  }
+});
