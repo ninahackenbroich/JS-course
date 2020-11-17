5216e78b3ddf20399579e774ec28e25c30c89c49
Nina Hackenbroich <hackenbroich.nina@gmail.com>
2020-11-17 14:20:09 +0100

    function returning functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greeterHey = greet('Hey');
+// greeterHey('Jonas');
+
+// greet('Hello')('Nina');
+
+// Transfer to Arrow Functions:
+
+const greet = greeting => {
+  return name => {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+
+greet('Hello')('Nina');
